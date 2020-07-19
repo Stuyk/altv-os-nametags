5a7abe8f6f0b280fb1d80002476cc65cd6ea5c14
Trevor Wessel <stuykgaming@gmail.com>
2020-07-19 16:05:20 -0600

    Increase Readability

--- client/startup.js
+++ client/startup.js
@@ -42,32 +42,52 @@ function drawNametags() {
     }
 
     for (let i = 0, n = alt.Player.all.length; i < n; i++) {
-        let player = alt.Player.all[i]
-        if (!player.valid) continue;
-        if (hideNametagsInVehicle && player.vehicle && alt.Player.local.vehicle !== player.vehicle) continue;
-        if (player.scriptID === alt.Player.local.scriptID) continue;
-        const name = player.getSyncedMeta('NAME')
-        if (!name) continue;
-        if (!native.hasEntityClearLosToEntity(alt.Player.local.scriptID, player.scriptID, 17)) continue;
-        let dist = distance2d(player.pos, alt.Player.local.pos)
-        if (dist > drawDistance) continue;
+        let player = alt.Player.all[i];
+        if (!player.valid) {
+            continue;
+        }
+
+        if (hideNametagsInVehicle && player.vehicle && alt.Player.local.vehicle !== player.vehicle) {
+            continue;
+        }
+
+        if (player.scriptID === alt.Player.local.scriptID) {
+            continue;
+        }
+
+        const name = player.getSyncedMeta('NAME');
+        if (!name) {
+            continue;
+        }
+
+        if (!native.hasEntityClearLosToEntity(alt.Player.local.scriptID, player.scriptID, 17)) {
+            continue;
+        }
+
+        let dist = distance2d(player.pos, alt.Player.local.pos);
+        if (dist > drawDistance) {
+            continue;
+        }
+
         const isChatting = player.getSyncedMeta('CHATTING');
+        const pos = { ...native.getPedBoneCoords(player.scriptID, 12844, 0, 0, 0) };
+        pos.z += 0.75;
 
+        let scale = 1 - (0.8 * dist) / drawDistance;
+        let fontSize = 0.6 * scale;
 
-        const pos = {...native.getPedBoneCoords(player.scriptID, 12844, 0, 0, 0)};
-        pos.z += 0.75
-        let scale = 1 - ((0.8 * dist) / drawDistance)
-        let fontSize = 0.6 * scale
         const lineHeight = native.getTextScaleHeight(fontSize, 4);
-
-
         const entity = player.vehicle ? player.vehicle.scriptID : player.scriptID;
         const vector = native.getEntityVelocity(entity);
         const frameTime = native.getFrameTime();
 
         // Names
-        native.setDrawOrigin(pos.x + (vector.x * frameTime), pos.y + (vector.y * frameTime),
-            pos.z + (vector.z * frameTime), 0);
+        native.setDrawOrigin(
+            pos.x + vector.x * frameTime,
+            pos.y + vector.y * frameTime,
+            pos.z + vector.z * frameTime,
+            0
+        );
         native.beginTextCommandDisplayText('STRING');
         native.setTextFont(4);
         native.setTextScale(fontSize, fontSize);
@@ -84,18 +104,18 @@ function drawNametags() {
         );
 
         // Bars
-        if (showBarsOnAim && foundEntity && aimingAtPed === player.scriptID) {
-            if (!native.isEntityDead(player.scriptID)) {
-                if (native.getEntityHealth(player.scriptID) > 0) {
-                    drawBarBackground(100, lineHeight, scale, 0.25, 139, 0, 0, 255)
-                    drawBar(native.getEntityHealth(player.scriptID) - 100, lineHeight, scale, 0.25, 255, 0, 0, 255)
-                }
-                if (native.getPedArmour(player.scriptID) > 0) {
-                    drawBarBackground(100, lineHeight, scale, 0.75, 140, 140, 140, 255)
-                    drawBar(native.getPedArmour(player.scriptID), lineHeight, scale, 0.75, 255, 255, 255, 255)
-                }
+        if (showBarsOnAim && foundEntity && aimingAtPed === player.scriptID && !native.isEntityDead(player.scriptID)) {
+            if (native.getEntityHealth(player.scriptID) > 0) {
+                drawBarBackground(100, lineHeight, scale, 0.25, 139, 0, 0, 255);
+                drawBar(native.getEntityHealth(player.scriptID) - 100, lineHeight, scale, 0.25, 255, 0, 0, 255);
+            }
+
+            if (native.getPedArmour(player.scriptID) > 0) {
+                drawBarBackground(100, lineHeight, scale, 0.75, 140, 140, 140, 255);
+                drawBar(native.getPedArmour(player.scriptID), lineHeight, scale, 0.75, 255, 255, 255, 255);
             }
         }
+
         native.clearDrawOrigin();
     }
 }
@@ -108,12 +128,11 @@ function distance2d(vector1, vector2) {
     return Math.sqrt(Math.pow(vector1.x - vector2.x, 2) + Math.pow(vector1.y - vector2.y, 2));
 }
 
-
 function drawBar(value, lineHeight, scale, position, r, g, b, a) {
     const healthWidth = value * 0.0005 * scale;
     native.drawRect(
-        (healthWidth - (100 * 0.0005 * scale)) / 2,
-        (lineHeight + (position * lineHeight)),
+        (healthWidth - 100 * 0.0005 * scale) / 2,
+        lineHeight + position * lineHeight,
         healthWidth,
         lineHeight / 4,
         r,
@@ -125,24 +144,6 @@ function drawBar(value, lineHeight, scale, position, r, g, b, a) {
 
 function drawBarBackground(value, lineHeight, scale, position, r, g, b, a) {
     const width = value * 0.0005 * scale;
-    native.drawRect(
-        0,
-        (lineHeight + (position * lineHeight)),
-        width + 0.002,
-        (lineHeight / 3) + 0.002,
-        0,
-        0,
-        0,
-        255
-    );
-    native.drawRect(
-        0,
-        (lineHeight + (position * lineHeight)),
-        width,
-        lineHeight / 3,
-        r,
-        g,
-        b,
-        a
-    );
+    native.drawRect(0, lineHeight + position * lineHeight, width + 0.002, lineHeight / 3 + 0.002, 0, 0, 0, 255);
+    native.drawRect(0, lineHeight + position * lineHeight, width, lineHeight / 3, r, g, b, a);
 }
